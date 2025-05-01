745cb492cd944c58ca95186e49c8e248db24baad
largo <largo@lukezautke.com>
2025-05-01 13:14:26 -0600

    config: add auxillary plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
+import Terminal from "vite-plugin-terminal";
+import svgr from 'vite-plugin-svgr'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    dts({
+      outDir: 'dist/types', // output directory for .d.ts files
+      tsconfigPath: './tsconfig.json',
+    }),
+    react(),
+    svgr(),
+    nodePolyfills(),
+    Terminal({
+      console: 'terminal',
+      output: ['terminal', 'console']
+    })
+  ],
 })
