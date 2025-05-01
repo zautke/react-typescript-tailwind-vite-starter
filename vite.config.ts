e2fc86620a11bc11cae6026b0515e6f75a484f22
largo <largo@lukezautke.com>
2025-05-01 13:14:26 -0600

    Initial skeleton

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-// vitest.config.js
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.js'],
-    css: true,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      exclude: [
-        'node_modules/',
-        'src/test/',
-        '**/*.d.ts',
-      ],
-    },
-  },
 })
